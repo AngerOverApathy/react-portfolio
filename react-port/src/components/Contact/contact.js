1885c86a9b23f97e4545e5160070109c5b6bd2db
AngerOverApathy <angersavestheworld@gmail.com>
2022-06-01 15:33:35 -0700

    contact updated

--- react-port/src/components/Contact/contact.js
+++ react-port/src/components/Contact/contact.js
@@ -1,9 +1,28 @@
 import React from "react";
+import React, { useState } from 'react';;
 
 function Contact() {
+    const [state, handleSubmit] = useForm("xknyyydk");
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
   return (
       <>
       <h1>Contact</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div className="form">
+          <label className="label" htmlFor="name">Name</label>
+          <input className="input" type="text" name="name" defaultValue={name} onBlur={handleChange} />
+        </div>
+        <div className="form">
+          <label className="label" htmlFor="email">Email Address</label>
+          <input className="input" type="email" name="email" defaultValue={email} onBlur={handleChange} />
+        </div>
+        <div className="form">
+          <label className="label" htmlFor="message">Message</label>
+          <textarea className="textarea" name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+        </div>
+        <button className="subBtn" data-testid="button" type="submit">Submit</button>
+      </form>
       </>
     );
   }
